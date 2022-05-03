5d571fb64b87f0294ecc320ff2cc849f173f1993
Travis <travis.moulton002@gmail.com>
2022-05-03 10:33:11 -0400

    added link to homepage to navbar

--- src/components/Navigation/Navbar/Navbar.tsx
+++ src/components/Navigation/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import { AppBar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
+import HomeIcon from '@mui/icons-material/Home';
 
 const StyledLink = styled(Link)`
   color: #fff;
   text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     text-decoration: underline;
@@ -15,7 +19,7 @@ interface IProps {
   isAuth: boolean;
 }
 
-export default function Navbar({ isAuth }: { isAuth: IProps['isAuth'] }) {
+export default function Navbar({ isAuth }: IProps) {
   return (
     <AppBar
       color="primary"
@@ -26,6 +30,9 @@ export default function Navbar({ isAuth }: { isAuth: IProps['isAuth'] }) {
         justifyContent: 'space-around',
       }}
     >
+      <StyledLink to="/">
+        <HomeIcon /> Home
+      </StyledLink>
       {!isAuth && <StyledLink to="/login">Login</StyledLink>}
       <StyledLink to="/add-todo">Add todo</StyledLink>
       {isAuth && <StyledLink to="/logout">Logout</StyledLink>}
